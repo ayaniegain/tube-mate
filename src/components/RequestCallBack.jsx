eab347434f031691adf2ce24c11f71bd8793dc36
ayaniegain <ayaniegain@gmail.com>
2023-12-08 17:00:13 +0530

    mn

--- src/components/RequestCallBack.jsx
+++ src/components/RequestCallBack.jsx
@@ -45,59 +45,72 @@ const handleClick=()=>{
         <button className="request-call-back-btn my=4"   data-bs-toggle="modal" data-bs-target="#exampleModal">Request a Call Back</button>
 {
 
-        !success ? 
+       ! success ? 
      
  <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
  <form  onSubmit={handleSubmit}>
 
-<div className="modal-dialog">
-  <div className="modal-content">
-    <div className="modal-header ">
-      <h5 className="modal-title mx-1" id="exampleModalLabel">Request a callback</h5>
-      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+ <div className="modal-dialog modal-dialog-centered" style={{ marginTop: '200px', height: '80%' }}>
+  <div className="modal-content bg-dark-color text-white">
+    <div className="modal-header border-0">
+      <h5 className="modal-title mx-1">Request a callback</h5>
+      <button type="button" className="btn-close btn-close-white " data-bs-dismiss="modal" aria-label="Close" />
     </div>
     <div className="modal-body">
-   <div className="xx ">
-<input required value={name} onChange={(e)=>setName(e.target.value)} type="text" className="form-control" placeholder="Enter Name" aria-label="Username" />
-<br/>
-<input required  value={phone} onChange={(e)=>setPhone(e.target.value)} type="number" className="form-control" placeholder="Enter Number" aria-label="Server" />
-</div>
-
-
+      <div className="xx">
+        <input
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          className="form-control custom-input"
+          placeholder="Enter Name"
+          aria-label="Username"
+        />
+        <br />
+        <input
+          required
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          type="number"
+          className="form-control custom-input"
+          placeholder="Enter Number"
+          aria-label="Server"
+        />
+      </div>
     </div>
-    <div className="modal-footer">
-      <button type="submit" className="btn btn-primary  rounded-pill m-auto ">Request a  callback</button>
+    <div className="modal-footer border-0">
+      <button type="submit" className="btn btn-primary rounded-pill m-auto bg-white text-black">
+        Request a callback
+      </button>
     </div>
   </div>
 </div>
+
+
+
 </form>
 
 </div>
 
         
         :
-
-
 <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
-   <div className="modal-dialog">
-     <div className="modal-content">
-     
-       <div className="modal-body">
-         <img src={correct} alt="logo" />
-         <h4>Request a call back</h4>
-         <h6>Our Team will call you shortly in <br/>
-12-24 hrs</h6>
-<h6>Can’t you wait for call?</h6>
-      
+  <div className="modal-dialog modal-dialog-centered modal-md mt-6">
+    <div className="modal-content bg-dark text-white">
+      <div className="modal-body text-center m-">
+        <img src={correct} alt="logo" className="mb-4" />
+        <h4 className="mb-2">Request a callback</h4>
+        <h6 style={{ color: '#808080' }} className="mb-4">Our Team will call you shortly in <br/>12-24 hrs</h6>
+        <h6 style={{ color: '#808080' }}>Can’t you wait for a call?</h6>
+      </div>
+      <div className="modal-footer d-flex justify-content-center border-0">
+        <button type="button" onClick={handleClick} className="btn btn-primary rounded-pill btn-wide">Check another video 👉</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-
-       </div>
-       <div className="modal-footer">
-         <button type="button" onClick={handleClick} className="btn btn-primary">checking another videos 👉 </button>
-       </div>
-     </div>
-   </div>
- </div>
 
 
 }
